89e128b7dc32e997c495626992df473bf3fc3510
jin <jinna0701@gmail.com>
2024-10-17 18:16:02 -0600

    LAB1

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        CPRG 303 To Do List
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
